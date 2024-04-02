f1cac6e844d6f04b10e0da5fb5ad666adab985ef
Dylan K. Taylor <dktapps@pmmp.io>
2024-04-02 15:43:07 +0100

    Release 0.3.0 (still experimental)

--- php_encoding.h
+++ php_encoding.h
@@ -6,7 +6,7 @@
 extern zend_module_entry encoding_module_entry;
 # define phpext_encoding_ptr &encoding_module_entry
 
-# define PHP_ENCODING_VERSION "0.2.4-dev"
+# define PHP_ENCODING_VERSION "0.3.0"
 
 # if defined(ZTS) && defined(COMPILE_DL_ENCODING)
 ZEND_TSRMLS_CACHE_EXTERN()
